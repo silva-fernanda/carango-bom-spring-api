413e33c3ef3e5d721e42f74486d5cce00e0253df
João Orlando <joao.ferreira@onebox.one>
2023-04-12 19:06:55 -0300

    limpando algumas partes do código, adicionando padrão DTO à marca e incluindo novo atributo

--- app/src/main/java/br/com/alura/app/marca/controller/MarcaController.java
+++ app/src/main/java/br/com/alura/app/marca/controller/MarcaController.java
@@ -1,7 +1,9 @@
 package br.com.alura.app.marca.controller;
 
+import br.com.alura.app.marca.dto.MarcaDTO;
 import br.com.alura.app.marca.entity.Marca;
 import br.com.alura.app.marca.service.MarcaService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,34 +13,30 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/marcas")
 public class MarcaController {
 
-    @Autowired
     private MarcaService marcaService;
 
-    //lista
     @GetMapping
     public List<Marca> listar() {
         return marcaService.listar();
     }
 
-    //exclui
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> excluir(@PathVariable Long id) {
         marcaService.excluir(id);
         return ResponseEntity.noContent().build();
     }
 
-    //cadastra
     @PostMapping
-    public ResponseEntity<Marca> cadastrar(@RequestBody Marca marca) {
-        Marca marcaSalva = marcaService.salvar(marca);
+    public ResponseEntity<Marca> cadastrar(@RequestBody MarcaDTO marcaDTO) {
+        Marca marcaSalva = marcaService.salvar(marcaDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(marcaSalva.getId()).toUri();
         return ResponseEntity.created(uri).body(marcaSalva);
     }
 
-    //atualiza
     @PutMapping("/{id}")
     public ResponseEntity<Marca> atualizar(@PathVariable Long id, @RequestBody Marca marca) {
         Marca marcaAtualizada = marcaService.atualizar(id, marca);
--- app/src/main/java/br/com/alura/app/marca/entity/Marca.java
+++ app/src/main/java/br/com/alura/app/marca/entity/Marca.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,6 +17,9 @@ public class Marca {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String nome;
+    @Size(max = 10000)
+    private String logotipo;
 
 }
