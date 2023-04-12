9b581a7549465e3e5de160ab76bbaff2b9a84b03
Fernanda Silva <fernanda.silva@onebox.one>
2023-04-12 18:39:07 -0300

    Atualizando nome de rotas

--- app/src/main/java/br/com/alura/app/veiculo/controller/VeiculoController.java
+++ app/src/main/java/br/com/alura/app/veiculo/controller/VeiculoController.java
@@ -11,27 +11,27 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/veiculos")
+@RequestMapping("api/veiculos")
 public class VeiculoController {
 
     @Autowired
     private VeiculoService veiculoService;
 
     //listar
-    @GetMapping
+    @GetMapping("/listarveiculos")
     public List<Veiculo> listar() {
         return veiculoService.listarVeiculosAVenda();
     }
 
     //excluir
-    @DeleteMapping("/{id}")
+    @DeleteMapping("excluirporid/{id}")
     public ResponseEntity<Void> excluir(@PathVariable Long id) {
         veiculoService.excluir(id);
         return ResponseEntity.noContent().build();
     }
 
     //cadastrar
-    @PostMapping
+    @PostMapping("/cadastrarveiculo")
     public ResponseEntity<Veiculo> cadastrar(@RequestBody Veiculo veiculo) {
         Veiculo veiculoSalvo = veiculoService.save(veiculo);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(veiculoSalvo.getId()).toUri();
@@ -39,7 +39,7 @@ public class VeiculoController {
     }
 
     //alterar
-    @PutMapping("/{id}")
+    @PutMapping("alterarveiculo/{id}")
     public ResponseEntity<Veiculo> atualizar(@PathVariable Long id, @RequestBody Veiculo veiculo) {
         Veiculo veiculoAtualizado = veiculoService.atualizar(id, veiculo);
         return ResponseEntity.ok(veiculoAtualizado);
